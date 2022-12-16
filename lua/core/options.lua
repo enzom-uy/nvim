2940c613f56a20b55f553e01fcfec6dc5eaf7e0b
enzom-uy <enzom.dev@gmail.com>
2022-12-16 13:15:58 -0300

    feat(config): add more config from previos setup

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,9 +1,10 @@
 vim.opt.shortmess:append { s = true, I = true } -- disable startup message
 astronvim.vim_opts(astronvim.user_plugin_opts("options", {
   opt = {
-    backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
+    backspace = vim.opt.backspace + { "nostop", "start", "eol", "indent" }, -- Don't stop backspace at insert
+    inccommand = "split",
     clipboard = "unnamedplus", -- Connection to the system clipboard
-    cmdheight = 0, -- hide command line unless needed
+    cmdheight = 1, -- hide command line unless needed
     completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
     copyindent = true, -- Copy the previous indentation on autoindenting
     cursorline = true, -- Highlight the text line of the cursor
@@ -13,29 +14,34 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     history = 100, -- Number of commands to remember in a history table
     ignorecase = true, -- Case insensitive searching
     laststatus = 3, -- globalstatus
+    shell = "zsh",
+    undodir = vim.fn.stdpath("data") .. "/undodir",
     lazyredraw = true, -- lazily redraw screen
     mouse = "a", -- Enable mouse support
     number = true, -- Show numberline
     preserveindent = true, -- Preserve indent structure as much as possible
+    breakindent = true,
     pumheight = 10, -- Height of the pop up menu
     relativenumber = true, -- Show relative numberline
-    scrolloff = 8, -- Number of lines to keep above and below the cursor
+    scrolloff = 10, -- Number of lines to keep above and below the cursor
     shiftwidth = 2, -- Number of space inserted for indentation
+    tabstop = 2, -- Number of space in a tab
     showmode = false, -- Disable showing modes in command line
     showtabline = 2, -- always display tabline
     sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
     signcolumn = "yes", -- Always show the sign column
     smartcase = true, -- Case sensitivie searching
+    smarttab = true,
     splitbelow = true, -- Splitting a new window below the current one
     splitright = true, -- Splitting a new window at the right of the current one
     swapfile = false, -- Disable use of swapfile for the buffer
-    tabstop = 2, -- Number of space in a tab
     termguicolors = true, -- Enable 24-bit RGB color in the TUI
     timeoutlen = 300, -- Length of time to wait for a mapped sequence
     undofile = true, -- Enable persistent undo
     updatetime = 300, -- Length of time to wait before triggering the plugin
     wrap = false, -- Disable wrapping of lines longer than the width of window
     writebackup = false, -- Disable making a backup before overwriting a file
+    cc = "100"
   },
   g = {
     highlighturl_enabled = true, -- highlight URLs by default
@@ -67,3 +73,28 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }))
+
+local disabled_built_ins = {
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "gzip",
+  "zip",
+  "zipPlugin",
+  "tar",
+  "tarPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "logipat",
+  "rrhelper",
+  "spellfile_plugin",
+  "matchit",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+  vim.g["loaded_" .. plugin] = 1
+end
