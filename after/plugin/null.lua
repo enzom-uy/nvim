b95546b6149d8749382fdad1408c2e6a78978f71
enzom-uy <enzom.dev@gmail.com>
2024-05-30 18:31:45 -0300

    Prettier stuff

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -41,3 +41,11 @@ null_ls.setup({
 		end
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	group = vim.api.nvim_create_augroup("RestartPrettierd", { clear = true }),
+	pattern = "*prettier*",
+	callback = function()
+		vim.fn.system("prettierd restart")
+	end,
+})
