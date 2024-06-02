f3c46b8b7cac60d88692c7b418d0f1d547f0f1d7
enzom-uy <enzom.dev@gmail.com>
2024-06-02 15:29:10 -0300

    Supermaven

--- after/plugin/supermaven.lua
+++ after/plugin/supermaven.lua
@@ -4,10 +4,10 @@ require("supermaven-nvim").setup({
 		clear_suggestion = "<C-]>",
 		accept_word = "<C-j>",
 	},
-	ignore_filetypes = {},
+	ignore_filetypes = { "neo-tree", "telescope" },
 	color = {
-		suggestion_color = "#ffffff",
-		cterm = 244,
+		-- suggestion_color = "#ffffff",
+		-- cterm = 244,
 	},
 	disable_inline_completion = false, -- disables inline completion for use with cmp
 	disable_keymaps = false, -- disables built in keymaps for more manual control
