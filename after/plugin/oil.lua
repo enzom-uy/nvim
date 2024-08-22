c709a42c33b38be131c6ddf1a05a2a4749690ead
enzom-uy <enzom.dev@gmail.com>
2024-08-22 15:43:15 -0300

    Oil.nvim - remove some conflicting keybinds

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -32,9 +32,6 @@ require("oil").setup({
 	keymaps = {
 		["g?"] = "actions.show_help",
 		["<CR>"] = "actions.select",
-		["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-		["<C-h>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
-		["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
 		["<C-p>"] = "actions.preview",
 		["<C-n>"] = "actions.close",
 		["<C-l>"] = "actions.refresh",
