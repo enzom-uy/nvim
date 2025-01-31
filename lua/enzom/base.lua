0d6d45c55e421dc1edd3e89186824fb9113526e1
enzom-uy <enzom.dev@gmail.com>
2025-01-30 21:25:41 -0300

    Update

--- lua/enzom/base.lua
+++ lua/enzom/base.lua
@@ -3,6 +3,7 @@ vim.cmd("autocmd!")
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
+vim.diagnostic.config({ virtual_text = true })
 
 vim.wo.number = true
 vim.wo.relativenumber = true
---
+++ lua/enzom/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				scss = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				yaml = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				timeout_ms = 300,
+				lsp_format = true,
+			},
+		})
+	end,
+}
